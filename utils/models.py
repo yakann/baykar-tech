be0e8ad28f28085b043475124eca958c2014b0b5
yakann <mahmut_yakan44@hotmail.com>
2022-11-02 22:36:40 +0300

    v0: Add rest_framework and user models

---
+++ utils/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class StarterModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
