b9cd295874c7d803f0852193349531fcb843f1ed
yakann <mahmut_yakan44@hotmail.com>
2022-11-03 09:44:33 +0300

    user_models: Add abstract model class and user models

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from enumfields import EnumField
+from users.enums import UserType
 from utils.models import StarterModel
-# Create your models here.
 
 
-class BaykarCustomer(AbstractUser, StarterModel):
+class BaykarCustomer(StarterModel, AbstractUser):
     email_allowed = models.BooleanField(default=False)
     sms_allowed = models.BooleanField(default=False)
-
+    user_type = EnumField(UserType, null=True, blank=True)
